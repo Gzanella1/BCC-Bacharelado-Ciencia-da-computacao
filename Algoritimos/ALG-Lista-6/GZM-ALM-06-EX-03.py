273de3270335521a6f801f76638db93a95c47cfe
Giovani  Zanella da Maia <96748771+Gzanella1@users.noreply.github.com>
2023-05-31 18:56:46 -0300

    Update GZM-ALM-06-EX-03.py
    exercício ok

--- Algoritimos/ALG-Lista-6/GZM-ALM-06-EX-03.py
+++ Algoritimos/ALG-Lista-6/GZM-ALM-06-EX-03.py
@@ -1,25 +1,38 @@
-def remove_extremos(lista, n):
-    # criar uma nova lista sem o n maiores
-    # e sem os n menores numeros da lista original
-    # exemplo: lista_original = [1, 2, 3, 4, 5, 6,7, 8, 9, 10]
-    # n = 2
-    nova_lista=sorted(lista)
-    for i in nova_lista(-n):
-            # nova_lista = [3, 4, 5, 6, 7,8]
-   
+""" 
+#ESSA FUNÇÃO RETORNA OS ULTIMOS N NUMEROS MAIORES/MENORES QUE O USUARIO DIGITAR
 
-    return nova_lista
+def remove_extremos(lista, sla):
+    nova_lista_ordenada=sorted(lista)
+    n = len(lista)
+    nova=nova_lista_ordenada[sla : n-sla]
+   for i in range(-sla,0):
+        nova.append(nova_lista_ordenada[i])
+    for i in range(0,sla):
+        nova.append(nova_lista_ordenada[i])
+
+        
+
+    #PEGA OS VALORES NO INTERVALO QUE O USUARIO DIGITAR 
+    lista = [0,1,2,3,4,5,6,7,8,9]
+    n = len(lista)
+    for i in range(sla, n-sla):
+        nova.append(nova_lista_ordenada[i])
+"""
+def remove_extremos(lista, sla):
+        return lista[sla: -sla]
+
 
 def main():
     lista = []
     while True:
         n = input("digite um numero (enter para finalizar):")
         if (n == ""):
+            x=int(input("Digite a quantidade de N maiores/menores que você quer: "))
             break
         else:
-            lista.append(int(n)
+            lista.append(int(n))
 
-                        )
-    
-    for i in lista:
-        print(i)
+    rm=remove_extremos(lista,x)
+    print(rm)
+
+main()
