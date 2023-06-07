f8e70c0d5de0f7c06c76cd3ebcccd8d21de0c21d
Giovani  Zanella da Maia <96748771+Gzanella1@users.noreply.github.com>
2023-06-07 17:54:31 -0300

    Update GZM-ALM-06-EX-11.PY

--- Algoritimos/ALG-Lista-6/11.py
+++ Algoritimos/ALG-Lista-6/11.py
@@ -1,14 +1,10 @@
 import random
 lista=[]
-novaLista=[]
-for i in range(6):
-    lista.append(random.randint(1,60))
-    for elemento in lista:
-            if (novaLista.count(elemento)==0):
-                novaLista.append(elemento)
-                aleatoria=random.randint(elemento)
-                novaLista.append(aleatoria)
-novaLista.sort()
-print(novaLista)
 
-
+while len(lista)<6:
+    n=random.randint(1,60)
+    if n not in lista:
+        lista.append(n)
+    
+lista.sort()
+print()
