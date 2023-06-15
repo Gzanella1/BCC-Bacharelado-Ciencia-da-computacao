17951af5b7d3047b396f07758123c751b18224d5
Giovani  Zanella da Maia <96748771+Gzanella1@users.noreply.github.com>
2023-06-15 14:31:20 -0300

    Create GZM-ALM-07-EX-04.py

---
+++ Algoritimos/ALG-Lista-7/GZM-ALM-07-EX-04.py
@@ -0,0 +1,18 @@
+def codifica_morse(palavra):
+    codigo_morse = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----'}
+    morse=""
+    for i in palavra:
+        if i.upper() in codigo_morse:
+            morse+=codigo_morse[i.upper()]+" "
+    return morse
+    
+
+
+def main():
+    palavra=input("Digite uma fraze para encriptar em codigo morse: ")
+    codifica=codifica_morse(palavra)
+    print(codifica)
+
+    
+main()
+
