068f94206097ecda12f46dcf940c77d971e5216c
Giovani  Zanella da Maia <96748771+Gzanella1@users.noreply.github.com>
2023-06-16 21:19:36 -0300

    Create GZM-ALM-08-EX-01.py

---
+++ Algoritimos/ALG-Lista-8/GZM-ALM-08-EX-01.py
@@ -0,0 +1,22 @@
+def fatorial(n):
+    if n>=0:
+        if n==1:
+            return n 
+        else:
+            fatorial=1
+            i=1
+            while i <=n:
+                fatorial=fatorial*(n-i)
+                i+=1
+            return fatorial
+    else:
+        return "numero invalido"
+
+
+
+def main():
+    n=int(input("digite um numero para saber o fatorial: "))
+    fatorial=fatorial(n)
+    print(fatorial)
+
+main()
